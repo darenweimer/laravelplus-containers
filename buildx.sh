d8c3b19b9fb7947adad022200ddfdb5e28123fec
darenweimer <darenweimer@gmail.com>
2023-06-21 01:44:24 -0700

    Updated tags for php containers

--- buildx.sh
+++ buildx.sh
@@ -51,14 +51,14 @@ container()
 
 create_builder
 
-container  alpine  nginx  production   laravelplus/nginx:1.24-alpine3.18   ALPINE_VERSION=3.18
-container  alpine  php    production   laravelplus/php:8.2-alpine3.18      ALPINE_VERSION=3.18  PHP_VERSION=82
-container  alpine  php    development  laravelplus/php:8.2-alpine3.18-dev  ALPINE_VERSION=3.18  PHP_VERSION=82
-container  alpine  node   production   laravelplus/node:18.16-alpine3.18   ALPINE_VERSION=3.18  NODE_PACKAGE=nodejs
+container  alpine  nginx  production   laravelplus/nginx:1.24-alpine3.18     ALPINE_VERSION=3.18
+container  alpine  php    production   laravelplus/php:8.2.7-alpine3.18      ALPINE_VERSION=3.18  PHP_VERSION=82
+container  alpine  php    development  laravelplus/php:8.2.7-alpine3.18-dev  ALPINE_VERSION=3.18  PHP_VERSION=82
+container  alpine  node   production   laravelplus/node:18.16-alpine3.18     ALPINE_VERSION=3.18  NODE_PACKAGE=nodejs
 
-container  ubuntu  nginx  production   laravelplus/nginx:1.18-ubuntu22.04   UBUNTU_VERSION=22.04
-container  ubuntu  php    production   laravelplus/php:8.2-ubuntu22.04      UBUNTU_VERSION=22.04  PHP_VERSION=8.2
-container  ubuntu  php    development  laravelplus/php:8.2-ubuntu22.04-dev  UBUNTU_VERSION=22.04  PHP_VERSION=8.2
-container  ubuntu  node   production   laravelplus/node:18.16-ubuntu22.04   UBUNTU_VERSION=22.04  NODE_VERSION=18
+container  ubuntu  nginx  production   laravelplus/nginx:1.18-ubuntu22.04     UBUNTU_VERSION=22.04
+container  ubuntu  php    production   laravelplus/php:8.2.7-ubuntu22.04      UBUNTU_VERSION=22.04  PHP_VERSION=8.2
+container  ubuntu  php    development  laravelplus/php:8.2.7-ubuntu22.04-dev  UBUNTU_VERSION=22.04  PHP_VERSION=8.2
+container  ubuntu  node   production   laravelplus/node:18.16-ubuntu22.04     UBUNTU_VERSION=22.04  NODE_VERSION=18
 
 delete_builder
